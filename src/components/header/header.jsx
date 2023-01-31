ccb2246c74d15608102389b640aa1492d958e84d
Natyes-ops <gurit.nat@bk.ru>
2023-01-31 11:32:58 +0300

    feat: add header

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,12 +1,16 @@
+import { Link } from 'react-router-dom';
+
 import { User } from '../user';
 
 import logo from './logo.svg';
+import menu from './Menu.svg';
 
 import './header.css';
 
 export const Header = () => (
     <header className='header'>
-        <img className='logo' src={logo} alt='Cleverland' />
+        <Link to="/" className='logo-link'><img className='logo' src={logo} alt='Cleverland' /></Link>
+        <img className='menu-mob' src={menu} alt='Cleverland' />
         <h3>Библиотека</h3>
         <User/>
     </header>
