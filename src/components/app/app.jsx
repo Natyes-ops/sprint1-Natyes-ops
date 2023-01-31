34b7ee6f87bae2ce24a0044014d35bde4ad21ce0
Natyes-ops <gurit.nat@bk.ru>
2023-01-31 11:36:49 +0300

    feat: add main page and data

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,9 +1,26 @@
+import { useState } from 'react';
+
+import { BookList } from '../book-list';
+import { Footer } from '../footer';
 import { Header } from '../header';
+import { MenuList } from '../menu';
+
+import { booksData, itemsData } from './data'
 
 import './app.css';
 
-export const App = () => (
-    <div className='container'>
-        <Header/>
-    </div>
-)
+export const App = () => {
+    const [items] = useState(itemsData);
+    const [books] = useState(booksData);
+
+    return(
+        <div className='container'>
+            <Header/>
+            <div className="main">
+                <MenuList items={items}/>
+                <BookList books={books}/>
+            </div>
+            <Footer/>
+        </div>
+    )
+}
--- src/index.js
+++ src/index.js
@@ -2,16 +2,28 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { HashRouter, Route, Routes } from 'react-router-dom';
 
+import { MenuList } from './components/menu';
+import { BookPage } from './pages/book';
+import { ContractPage } from './pages/contract';
 import { MainPage } from './pages/main';
+import { TermsPage } from './pages/terms';
 
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
+  // eslint-disable-next-line react/jsx-filename-extension
   <React.StrictMode>
     <HashRouter>
       <Routes>
-        <Route path='/' element={<MainPage />} />
+        <Route path='/' element={<MainPage />} >
+          <Route path='/' element={<MenuList to='/books/all'/>} />
+          <Route path='/books/:category' element={<BookPage />} />
+          <Route path='/terms' element={<TermsPage />} />
+          <Route path='/contract' element={<ContractPage />} />
+        </Route>
+        <Route path='/books/:category/:bookId' element={<BookPage />} />
       </Routes>
     </HashRouter>
   </React.StrictMode>
