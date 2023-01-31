a88535a6810c1ceca34e4c9a3998c7f7505e5873
Natyes-ops <gurit.nat@bk.ru>
2023-01-31 22:44:38 +0300

    feat: change package

--- .eslintrc.js
+++ .eslintrc.js
@@ -23,8 +23,8 @@ module.exports = {
         'import/no-default-export': 'error',
         indent: 'off', // https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/docs/rules/indent.md
         '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^action' }],
-        "no-nested-ternary": "off",
-        "no-unneeded-ternary": "off"
+        'no-nested-ternary': 'off',
+        'no-unneeded-ternary': 'off'
     },
     ignorePatterns: ['coverage', 'cypress.config.ts'],
 };
