8021d4389b40c2a8c86c95f782f0bfdf0d19e424
Natyes-ops <gurit.nat@bk.ru>
2023-01-31 20:50:48 +0300

    feat: add book-page

--- src/pages/book/book-page.jsx
+++ src/pages/book/book-page.jsx
@@ -1,4 +1,28 @@
-export const BookPage = () => (
-    <section className='book-page'>
-    </section>
+import { useLocation } from 'react-router-dom';
+
+import { BookInfo } from '../../components/book-info';
+import { Crumbs } from '../../components/crumbs';
+import { Header } from '../../components/header';
+
+import './book-page.css';
+
+export const BookPage = () => {
+    const {state} = useLocation();
+
+    // eslint-disable-next-line no-console
+    console.log(state);
+    
+    return(
+    <div className="book-page">
+        <div className="container">
+            <Header/>
+        </div>
+        <Crumbs link={state.propsBook.title} category={state.propsBook.categoryRu}/>
+        <main className="container book-main">
+            <BookInfo date={state}/>
+        </main>
+    </div>
+
 );
+}
+ 
