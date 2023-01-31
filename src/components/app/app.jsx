319497379ea7a7fa137a3d4563a00101de1b1f7a
Natyes-ops <gurit.nat@bk.ru>
2023-01-31 21:54:25 +0300

    fix: change app

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,9 +1,12 @@
 import { useState } from 'react';
+import { useLocation } from 'react-router-dom';
 
 import { BookList } from '../book-list';
+import { ContractPage } from '../contract';
 import { Footer } from '../footer';
 import { Header } from '../header';
 import { MenuList } from '../menu';
+import { TermsPage } from '../terms';
 
 import { booksData, itemsData } from './data'
 
@@ -12,13 +15,15 @@ import './app.css';
 export const App = () => {
     const [items] = useState(itemsData);
     const [books] = useState(booksData);
+    const root = useLocation();
+    const activeLink = () => root.pathname ===  '/' ? <BookList books={books}/> : root.pathname ===  '/terms' ? <TermsPage/> : root.pathname ===  '/contract' ? <ContractPage/> : <BookList books={books}/>;
 
     return(
         <div className='container'>
             <Header/>
             <div className="main">
                 <MenuList items={items}/>
-                <BookList books={books}/>
+                {activeLink()}
             </div>
             <Footer/>
         </div>
